d2361b46830f613a44c28a1157d6241a05b367e2
ankonzoid <anson.ao@gmail.com>
2017-09-27 16:05:54 -0400

    update

--- hunterprey/hunterprey.py
+++ hunterprey/hunterprey.py
@@ -6,19 +6,29 @@
 
 """
 import numpy as np
-import random
-import itertools
-import operator
+import random, itertools, operator, sys, os
+
+sys.path.append("../src")
+from AgentClass import Agent
+from BrainClass import Brain
+from EnvironmentClass import Environment
+from MemoryClass import Memory
+
 
 def main():
     print("Hunter-Prey world")
+
+    # Settings
     hunter_agent_info = {"epsilon": 0.2}
     prey_agent_info = {"epsilon": 1.0}
+    env_info = {"Ny": 7, "Nx": 7}
 
-
+    # Set up agent and environment
     hunter_agent = Agent(hunter_agent_info)
     prey_agent = Agent(prey_agent_info)
-
+    env = Environment(env_info)
+    brain = Brain(env)
+    memory = Memory(env)
 
 
 
