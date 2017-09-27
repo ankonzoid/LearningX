57c7f615c872a626f86a0e7f5d34acbe24e47b21
ankonzoid <anson.ao@gmail.com>
2017-09-27 16:01:03 -0400

    update

--- hunterprey/hunterprey.py
+++ hunterprey/hunterprey.py
@@ -11,7 +11,18 @@ import itertools
 import operator
 
 def main():
-    print("Hunter prey")
+    print("Hunter-Prey world")
+    hunter_agent_info = {"epsilon": 0.2}
+    prey_agent_info = {"epsilon": 1.0}
+
+
+    hunter_agent = Agent(hunter_agent_info)
+    prey_agent = Agent(prey_agent_info)
+
+
+
+
+
 
 # Driver 
 if __name__ == '__main__':
