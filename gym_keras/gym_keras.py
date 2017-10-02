585d940baa0d9849fbd499e2ac08473ae5a42206
ankonzoid <anson.ao@gmail.com>
2017-10-02 12:03:15 -0400

    update

--- gym_keras/gym_keras.py
+++ gym_keras/gym_keras.py
@@ -22,25 +22,22 @@ class Agent():
         self.gradients = []
         self.rewards = []
         self.probs = []
-        self.model = self._build_model(env)
-
-        self.Q = 0
 
 
-    def _build_model(self, env):
-        model = Sequential()
-        model.add(Reshape((1, env.Ny, env.Nx), input_shape=(self.state_size,)))
-        model.add(Convolution2D(32, (6, 6), strides=(3, 3), padding="same",
+        self.Q = self._build_Q(env)
+
+    def _build_Q(self, env):
+        Q = Sequential()
+        Q.add(Reshape((1, env.Ny, env.Nx), input_shape=(self.state_size,)))
+        Q.add(Convolution2D(32, (6, 6), strides=(3, 3), padding="same",
                                 activation="relu", kernel_initializer="he_uniform"))
-        model.add(Flatten())
-        model.add(Dense(64, activation="relu", kernel_initializer="he_uniform"))
-        model.add(Dense(32, activation="relu", kernel_initializer="he_uniform"))
-        model.add(Dense(self.action_size, activation="softmax"))
+        Q.add(Flatten())
+        Q.add(Dense(64, activation="relu", kernel_initializer="he_uniform"))
+        Q.add(Dense(32, activation="relu", kernel_initializer="he_uniform"))
+        Q.add(Dense(self.action_size, activation="softmax"))
         opt = Adam(lr=self.learning_rate)
-        model.compile(loss="categorical_crossentropy", optimizer=opt)
-        return model
-
-
+        Q.compile(loss="categorical_crossentropy", optimizer=opt)
+        return Q
 
     # ===================
     # Training functions
@@ -48,7 +45,7 @@ class Agent():
 
     def get_action(self, state):
         state = state.reshape([1, state.shape[0]])
-        aprob = self.model.predict(state, batch_size=1).flatten()
+        aprob = self.Q.predict(state, batch_size=1).flatten()  # forward pass
         self.probs.append(aprob)
         prob = aprob / np.sum(aprob)
         action = np.random.choice(self.action_size, 1, p=prob)[0]
@@ -70,20 +67,24 @@ class Agent():
 class Environment():
     def __init__(self):
 
-        self.grid = np.zeros((100,100))
-        self.grid[:10, :10] = 1
+        self.Ny = 100
+        self.Nx = 100
 
         self.actions = np.arange(4)
 
-        self.Ny = 100
-        self.Nx = 100
-        self.state_dim = self.grid.shape[0] * self.grid.shape[1]
+        self.state_dim = self.Ny * self.Nx
         self.action_dim = len(self.actions)
+
+        self.reward = self._define_reward()
+
+    def _define_reward(self):
+        reward = np.zeros((self.Ny, self.Nx))
+        reward[(self.Ny-10):, (self.Nx-10):] = 100
+        return reward
 
     def starting_state(self):
         state = [0, 0]
         return state
-
 
     def get_reward(self, state, action):
         reward = state + action
@@ -99,8 +100,8 @@ def main():
     env = Environment()
     agent = Agent(env)
 
-
-    plt.imshow(env.grid)
+    plt.figure(figsize=(8, 8))
+    plt.imshow(env.reward)
     plt.show()
     
     exit()
@@ -109,14 +110,16 @@ def main():
 
     state = env.starting_state()
     for episode in range(N_episodes_train):
+
         print("episode {}".format(episode))
-        action, prob = agent.get_action(state)
-        reward = env.get_reward(state, action)
-        state_new = env.perform_action(state, action)
 
-        agent.update(state, state_new, action, reward)
+        action, prob = agent.get_action(state)  # pick action
+        reward = env.get_reward(state, action)  # collect reward
+        state_new = env.perform_action(state, action)  # observe next state
 
-        state = state_new
+        agent.update(state, state_new, action, reward)  # update Q
+
+        state = state_new  # transition to next state
 
 
 
